eed5c8ee42cab8edae6b33d05154c7537dd60123
Simone Garnero <simonegarnero2gmail.com>
2021-05-07 16:58:43 +0200

    Aggiunta la possibilità di scelta tra stampa semplice e a griglia e creazione file .cpp da riempire, mancano alcuni ritocch ma per ora funziona

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+#include <cassert>
+#include <iostream>
+
 #include "newSIR.hpp"
 
 int main() {
   epidemia::SIR mondo = epidemia::insert();
   mondo.riempimento();
-  mondo.print();
+
+  int x;
+  std::cout << "Preferisce una rappresentazione grafica a griglia oppure una visualizzazione minimale dei dati?\n"
+               "Per la rappresentazione a schermo di una griglia premere 1, in caso contrario premere 0 \n";
+  std::cin >> x;
+  assert(x == 0 || x == 1);         //Da mettere un'exception al posto dell'assert
+  if (x == 1) {
+    mondo.print();
+  } else if (x == 0) {
+    mondo.print_semplice();
+  }
 }
