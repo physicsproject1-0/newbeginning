51155908ff188b08b129be77a5815e83d04dee73
Edoardo Frulla <edofru01@gmail.com>
2021-05-14 03:02:23 +0200

    modifiche mezze iniziate

--- grafica/classe.hpp
+++ grafica/classe.hpp
@@ -5,11 +5,16 @@
 #define DISA
 
 class Persona {
-  sf::VertexArray triangolo;  //  perchè cazzo non funziona
+  sf::Vector2f centro;
+  sf::Vector2f raggio;
   sf::Vector2f vel;
+  sf::VertexArray triangolo;  //  perchè cazzo non funziona
 
  public:
-  Persona(sf::Vector2f const& centro, float radius) {
+  Persona(sf::Vector2f const& center, float radius) {
+    centro = center;
+    raggio = radius;
+    vel = sf::Vector2f(rand() % 5 - 2.5f, rand() % 5 - 2.5f);
     triangolo.setPrimitiveType(sf::Triangles);
     triangolo.resize(3);
     triangolo[0].position = sf::Vector2f(centro.x, centro.y - radius);  // strane coord
@@ -18,8 +23,6 @@ class Persona {
     triangolo[0].color = sf::Color::Yellow;
     triangolo[1].color = sf::Color::Yellow;
     triangolo[2].color = sf::Color::Yellow;
-
-    vel = sf::Vector2f(rand() % 5 - 2.5f, rand() % 5 - 2.5f);
   }
   sf::Vector2f get(int n) { return triangolo[n].position; }
   sf::Vector2f getv() { return vel; }
@@ -28,19 +31,19 @@ class Persona {
 class Mondo : public sf::Drawable {
  private:  // la draw non va nel protected??
   sf::VertexArray Griglia;
-  int personetotali;
-  std::vector<sf::Vector2f> velocita;
+  
+  std::vector<Persona> Lista;
   virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const { target.draw(Griglia); }
   // perchè la devo mettere qua
   // virtual qua non ce lo devo mettere, giusto? tanto non creerò classi derivate da questa
   // poi come fa window.draw(entity) a chiamare internamente entity.draw se è nella parte privata?!
- 
- void invvel(int i){
-   velocita[i] = -velocita[i];
- }
+
+  void invvel(int i) { velocita[i] = -velocita[i]; }
+
  public:
   Mondo(int persone) {
-    personetotali = persone;
+    Persona prova;
+    Lista=std::vector<Persona>(persone, prova);//appena aggiunto
     Griglia.resize(persone * 3);
     Griglia.setPrimitiveType(sf::Triangles);
     for (int i = 0; i < persone; i++) {
@@ -52,15 +55,14 @@ class Mondo : public sf::Drawable {
       velocita.push_back(test.getv());
     }
   }
+  void //modificare la classeeeeeeeeee
   void evolvi() {
-
     for (int i = 0; i < personetotali * 3; i++) {
-      if (Griglia[i].position.x >550||Griglia[i].position.x<-50 || Griglia[i].position.y<-50||Griglia[i].position.y>550){
+      if (Griglia[i].position.x > 550 || Griglia[i].position.x < -50 || Griglia[i].position.y < -50 || Griglia[i].position.y > 550) {
         invvel(i);
       }
-      Griglia[i].position = sf::Vector2f(Griglia[i].position.x + velocita[(int)(i/3)].x, Griglia[i].position.y + velocita[(int)(i/3)].x);
+      Griglia[i].position = sf::Vector2f(Griglia[i].position.x + velocita[(int)(i / 3)].x, Griglia[i].position.y + velocita[(int)(i / 3)].x);
     }
-
   }
 };
 
