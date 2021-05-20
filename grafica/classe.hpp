99243c7cec6f10496ba026c91611975fcb6267a2
Edoardo Frulla <edofru01@gmail.com>
2021-05-20 15:25:34 +0200

    roba

--- grafica/classe.hpp
+++ grafica/classe.hpp
@@ -7,7 +7,7 @@
 struct Persona {
   sf::Vector2f centro;
   float raggio;
-  sf::Vector2f vel ;
+  sf::Vector2f vel;
 };
 
 class Mondo : public sf::Drawable {
@@ -27,12 +27,12 @@ class Mondo : public sf::Drawable {
  public:
   Mondo(int persone) {
     Persona prova;
-    
-    for (int i = 0; i<persone;i++){
-    prova.centro = sf::Vector2f(rand() % 500, rand() % 500);
-    prova.raggio = 10.f;
-    prova.vel = sf::Vector2f(rand() % 50 - 25.f, rand() % 50 - 25.f);
-    Lista[i]=prova;
+
+    for (int i = 0; i < persone; i++) {
+      prova.centro = sf::Vector2f(rand() % 450 + 50, rand() % 450 + 50);
+      prova.raggio = 10.f;
+      prova.vel = sf::Vector2f(rand() % 50 - 25.f, rand() % 50 - 25.f);
+      Lista[i] = prova;
     }
     Griglia.resize(Lista.size() * 3);
     Griglia.setPrimitiveType(sf::Triangles);
@@ -51,31 +51,39 @@ class Mondo : public sf::Drawable {
       iter[0].position = sf::Vector2f(Lista[i].centro.x, Lista[i].centro.y - Lista[i].raggio);  // strane coord
       iter[1].position = sf::Vector2f(Lista[i].centro.x - Lista[i].raggio * (1.7f / 2), Lista[i].centro.y + (Lista[i].raggio / 2));
       iter[2].position = sf::Vector2f(Lista[i].centro.x + Lista[i].raggio * (1.7f / 2), Lista[i].centro.y + (Lista[i].raggio / 2));
-      
     }
   }
 
-  void evolvi_singolo(int indice){
-    
-    Lista[indice].centro += Lista[indice].vel*trascorso.asSeconds();
+  //sf::Vector2f estimate(Persona& persona) {
+  //  return persona.centro + persona.vel * trascorso.asSeconds();
+  //  // (estimated.x <50 || estimated.y<50 || estimated.y>500 || estimated.x>500){
+  //}
+
+//pensare a modo con cambiare la posizione delle pallli ne che sbattono
+  void evolvi_singolo(int indice) {
+    //while (estimate(Lista[indice]) e altro) {
+    //}
+
+    Lista[indice].centro += Lista[indice].vel * trascorso.asSeconds();
 
     sf::Vertex* iter = &Griglia[indice * 3];
-      iter[0].position += sf::Vector2f(Lista[indice].vel*trascorso.asSeconds());  // strane coord
-      iter[1].position += sf::Vector2f(Lista[indice].vel*trascorso.asSeconds());
-      iter[2].position += sf::Vector2f(Lista[indice].vel*trascorso.asSeconds());
-
+    iter[0].position += sf::Vector2f(Lista[indice].vel * trascorso.asSeconds());  // strane coord
+    iter[1].position += sf::Vector2f(Lista[indice].vel * trascorso.asSeconds());
+    iter[2].position += sf::Vector2f(Lista[indice].vel * trascorso.asSeconds());
   }
+
 
   void evolvi() {
-    for (int i = 0; i < Lista.size(); i++) {
-      // if (Griglia[i].position.x > 550 || Griglia[i].position.x < -50 || Griglia[i].position.y < -50 || Griglia[i].position.y > 550) {
-      //  invvel(i);
-      //}
-      evolvi_singolo(i);
-    }
+  for (int i = 0; i < Lista.size(); i++) {
+    // if (Griglia[i].position.x > 550 || Griglia[i].position.x < -50 || Griglia[i].position.y < -50 || Griglia[i].position.y > 550) {
+    //  invvel(i);
+    //}
+    evolvi_singolo(i);
   }
+}
 
-  void azzera() { trascorso = timer.restart(); }
-};
+void azzera() { trascorso = timer.restart(); }
+}
+;
 
 #endif
