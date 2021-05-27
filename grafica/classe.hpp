0ef43f64a2965fb84303a452e6cb99b64dea72c2
Edoardo Frulla <edofru01@gmail.com>
2021-05-27 17:52:48 +0200

    loading texture

--- grafica/classe.hpp
+++ grafica/classe.hpp
@@ -8,21 +8,26 @@
 
 struct Persona {
   sf::Vector2f centro;
-  // float raggio;
+  float raggio;
   sf::Vector2f vel;
   sf::Clock cambiovelocita;
-  float metalato;
+  // float metalato;
 };
 
 class Mondo : public sf::Drawable {
  private:  // la draw non va nel protected??
   sf::VertexArray Griglia;
+
   std::map<int, Persona> Lista;  // contiene solo le persone
 
   sf::Clock timer;
   sf::Time trascorso;
-
-  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const { target.draw(Griglia); }
+  sf::Texture ominoprova;
+  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    states.texture = &ominoprova;
+    //capire che madonna succede qui dentro!!!
+    target.draw(Griglia, states);
+  }
 
   // perchè la devo mettere qua
   // virtual qua non ce lo devo mettere, giusto? tanto non creerò classi derivate da questa
@@ -30,19 +35,23 @@ class Mondo : public sf::Drawable {
 
  public:
   Mondo(int persone) {
+    if (!ominoprova.loadFromFile("uomotest.png")) {
+      throw std::runtime_error{"texture loading failed"};
+    }
+
     Persona prova;
 
     for (int i = 0; i < persone; i++) {
       prova.centro = sf::Vector2f(rand() % 450 + 50, rand() % 450 + 50);
-      // prova.raggio = 10.f;
-      prova.metalato = 5.f;
+      prova.raggio = 10.f;
+      // prova.metalato = 5.f;
       prova.vel = sf::Vector2f(rand() % 50 - 25.f, rand() % 50 - 25.f);
       Lista[i] = prova;
     }
-    // Griglia.resize(Lista.size() * 3);
-    Griglia.resize(Lista.size() * 4);
-    Griglia.setPrimitiveType(sf::Quads);
-    // Griglia.setPrimitiveType(sf::Triangle);
+    Griglia.resize(Lista.size() * 3);
+    // Griglia.resize(Lista.size() * 4);
+    // Griglia.setPrimitiveType(sf::Quads);
+    Griglia.setPrimitiveType(sf::Triangles);
   }
 
   void aggiornagriglia() {
@@ -52,15 +61,18 @@ class Mondo : public sf::Drawable {
       // iter[0].color = sf::Color::Yellow;
       // iter[1].color = sf::Color::Yellow;
       // iter[2].color = sf::Color::Yellow;
-      // sf::Vertex* iter = &Griglia[i * 3];
-      // iter[0].position = sf::Vector2f(Lista[i].centro.x, Lista[i].centro.y - Lista[i].raggio);  // strane coord
-      // iter[1].position = sf::Vector2f(Lista[i].centro.x - Lista[i].raggio * (1.7f / 2), Lista[i].centro.y + (Lista[i].raggio / 2));
-      // iter[2].position = sf::Vector2f(Lista[i].centro.x + Lista[i].raggio * (1.7f / 2), Lista[i].centro.y + (Lista[i].raggio / 2));
-      sf::Vertex* iter = &Griglia[i * 4];
-      iter[0].position = sf::Vector2f(Lista[i].centro.x - Lista[i].metalato, Lista[i].centro.y - Lista[i].metalato);  // strane coord
-      iter[1].position = sf::Vector2f(Lista[i].centro.x + Lista[i].metalato, Lista[i].centro.y - Lista[i].metalato);
-      iter[2].position = sf::Vector2f(Lista[i].centro.x + Lista[i].metalato, Lista[i].centro.y + Lista[i].metalato);
-      iter[3].position = sf::Vector2f(Lista[i].centro.x - Lista[i].metalato, Lista[i].centro.y + Lista[i].metalato);
+      sf::Vertex* iter = &Griglia[i * 3];
+      iter[0].position = sf::Vector2f(Lista[i].centro.x, Lista[i].centro.y - Lista[i].raggio);  // strane coord
+      iter[1].position = sf::Vector2f(Lista[i].centro.x - Lista[i].raggio * (1.7f / 2), Lista[i].centro.y + (Lista[i].raggio / 2));
+      iter[2].position = sf::Vector2f(Lista[i].centro.x + Lista[i].raggio * (1.7f / 2), Lista[i].centro.y + (Lista[i].raggio / 2));
+      iter[0].texCoords = sf::Vector2f(430.f, 0.f);  // strane coord
+      iter[1].texCoords = sf::Vector2f(0.f, 1681.f);
+      iter[2].texCoords = sf::Vector2f(860.f, 1681.f);
+      // sf::Vertex* iter = &Griglia[i * 4];
+      // iter[0].position = sf::Vector2f(Lista[i].centro.x - Lista[i].metalato, Lista[i].centro.y - Lista[i].metalato);  // strane coord
+      // iter[1].position = sf::Vector2f(Lista[i].centro.x + Lista[i].metalato, Lista[i].centro.y - Lista[i].metalato);
+      // iter[2].position = sf::Vector2f(Lista[i].centro.x + Lista[i].metalato, Lista[i].centro.y + Lista[i].metalato);
+      // iter[3].position = sf::Vector2f(Lista[i].centro.x - Lista[i].metalato, Lista[i].centro.y + Lista[i].metalato);
     }
   }
 
@@ -80,7 +92,7 @@ class Mondo : public sf::Drawable {
     if (Lista[indice].centro.y < 50 || Lista[indice].centro.y > 550) {
       Lista[indice].vel.y = -Lista[indice].vel.y;
     }
-    check_collisions(indice);
+    // check_collisions(indice);
     // if (Lista[indice].cambiovelocita.getElapsedTime().asSeconds() > 3) {  // per modificare il moto browniano
     //  sf::Vector2f nuovavel(rand() % 50 - 25.f, rand() % 50 - 25.f);
     //  Lista[indice].vel = nuovavel;
@@ -114,16 +126,16 @@ class Mondo : public sf::Drawable {
   }  // in caso creare delle corone circolari con varie numerazioni
   // introdurre dipendenza dal tempo
 
-  void check_collisions(int j) {
-    for (int i = 0; i < Lista.size(); i++) {
-      if (((abs(Lista[i].centro.x - Lista[j].centro.x) <= 2 * Lista[j].metalato)) &&
-          (abs(Lista[i].centro.y - Lista[j].centro.y) <= 2 * Lista[j].metalato) && (i != j)) {
-        std::swap(Lista[j].vel , Lista[i].vel);
-
-        
-      }
-    }
-  }
+  // void check_collisions(int j) {
+  //  for (int i = 0; i < Lista.size(); i++) {
+  //    if (((abs(Lista[i].centro.x - Lista[j].centro.x) <= 2 * Lista[j].metalato)) &&
+  //        (abs(Lista[i].centro.y - Lista[j].centro.y) <= 2 * Lista[j].metalato) && (i != j)) {
+  //      std::swap(Lista[j].vel , Lista[i].vel);
+  //
+  //
+  //    }
+  //  }
+  //}
 
   double modulo(sf::Vector2f const& vettore) { return sqrt(pow(vettore.x, 2) + pow(vettore.y, 2)); }
 
--- grafica/mainfake.cpp
+++ grafica/mainfake.cpp
@@ -1,31 +1,31 @@
-#include "classe.hpp"
 #include <iostream>
 
-int main(){
-    sf::RenderWindow window(sf::VideoMode(600, 600), "Tilemap");
-    window.setFramerateLimit(60);
-    int n;
-    std::cin>>n;
-    Mondo map(n);
-    
-    while (window.isOpen())
-    {
-        // handle events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-                window.close();
-        }
+#include "classe.hpp"
+//#include <SFML/OpenGL.hpp>
 
-        // draw the map
-        window.clear();
+int main() {
+  sf::RenderWindow window(sf::VideoMode(600, 600), "Tilemap");
+  window.setFramerateLimit(60);
+  
+  int n;
+  std::cin >> n;
+  Mondo map(n);
 
-        map.azzera();
-        map.evolvi();
-        map.aggiornagriglia();
-        window.draw(map);
-        window.display();
+  while (window.isOpen()) {
+    // handle events
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) window.close();
     }
 
+    // draw the map
+    window.clear();
+
+    map.azzera();
+    map.evolvi();
+    map.aggiornagriglia();
+    window.draw(map);
+    window.display();
+    // glFinish();
+  }
 }
