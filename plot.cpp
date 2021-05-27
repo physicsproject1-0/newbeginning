1caf1bab9a7508587f64fb2be2c1de761989e31a
Simone Piraccini <simopira01@gmail.com>
2021-05-27 18:46:51 +0200

    iniziamo gnuplot

--- plot.cpp
+++ plot.cpp
@@ -1,3 +1,11 @@
+#include <vector>
+
 #include "gnuplot-iostream.h"
 
-
+int main() {
+  Gnuplot g1;
+  g1 << "set terminal jpeg\n";
+  g1 << "set output 'prova.jpeg'\n";
+  g1 << "set xrange [0:10] \n set yrange [0:100]\n";   // 10 numero dei giorni di simulazione
+  g1 << "plot x+2";
+}
