3be553b1deb0b2f36d7ff1f28c01472c3e035691
Simone Garnero <simonegarnero2gmail.com>
2021-05-31 20:18:29 +0200

    Colori aggiustati, ora più scuri e godibili, settate le linee e tolti i punti dopo una miriade di prove, penso sia la scelta migliore

--- plot.cpp
+++ plot.cpp
@@ -8,13 +8,11 @@ namespace epidemia {
 void grafico(std::vector<double> x, std::vector<double> y, std::vector<double> z) {
   Gnuplot g1;
 
-  // g1 << "set terminal jpeg\n";
-  // g1 << "set output 'prova.jpeg'\n";      
   g1 << "set title 'Modello SIR'\n";
-  g1 << "set xrange [0:*] \n";                    //Da risolvere warning del range
-  g1 << "plot" << g1.file1d(x) << "with linespoints title 'Suscettibili' linecolor rgb 'green'," << g1.file1d(y) 
-     << "with linespoints title 'Infetti' linecolor rgb 'yellow'," << g1.file1d(z)
-     << "with linespoints title 'Rimossi' linecolor rgb 'red'" << std::endl;
-} 
+  g1 << "set xrange [0:*] \n";        
+  g1 << "plot" << g1.file1d(x) << "with lines title 'Suscettibili' linecolor rgb 'dark-green' linewidth 2," << g1.file1d(y)
+     << "with lines title 'Infetti' linecolor rgb 'dark-yellow' linewidth 2," << g1.file1d(z)
+     << "with lines title 'Rimossi' linecolor rgb 'dark-red' linewidth 2" << std::endl;
+}
 
-}  // namespace epidemia 
+}  // namespace epidemia
