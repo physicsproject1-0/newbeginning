22d4080005feeca09b7f75880bf2ce6a07241caa
Simone Piraccini <simopira01@gmail.com>
2021-08-20 10:58:55 +0200

    titoli degli assi

--- plot.cpp
+++ plot.cpp
@@ -1,5 +1,4 @@
-#include <vector>
-
+//#include <vector>   /// perchè avevamo inserito anche vector? senza funziona ugualmente
 #include "gnuplot-iostream.h"
 #include "newSIR.hpp"
 
@@ -9,6 +8,7 @@ void grafico(std::vector<double> x, std::vector<double> y, std::vector<double> z
   Gnuplot g1;
 
   g1 << "set title 'Modello SIR'\n";
+  g1 << "set xlabel 'Tempo (giorni)'\n set ylabel 'Parametri (unità)'\n";
   g1 << "set xrange [0:*] \n";        
   g1 << "plot" << g1.file1d(x) << "with lines title 'Suscettibili' linecolor rgb 'dark-green' linewidth 2," << g1.file1d(y)
      << "with lines title 'Infetti' linecolor rgb 'dark-yellow' linewidth 2," << g1.file1d(z)
