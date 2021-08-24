20cee99f24b320c96b4d73f14dec8e69dea5b508
Simone Piraccini <simopira01@gmail.com>
2021-08-24 13:16:42 +0200

    Scritto il primo test

--- testSIR.cpp
+++ testSIR.cpp
@@ -4,10 +4,16 @@
 #include "newSIR.hpp"
 
 TEST_CASE("Testing State") {
-  std::vector<epidemia::State> prova;
-  // bisogna riempirlo
-
-  CHECK();
-  CHECK();
-  CHECK();
+  epidemia::State start {100, 10, 0, 0};
+  int giorni = 30;
+  double beta = 0;
+  double gamma = 0;
+  int const people = 110;
+  epidemia::SIR test { start, giorni, beta, gamma, people};
+  
+  auto risultato = test.convertitore(test.riempimento());
+  epidemia::State finale = risultato.back();
+  CHECK(finale.infetti == start.infetti);
+  //CHECK();
+  //CHECK();
 }
