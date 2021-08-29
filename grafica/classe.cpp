5bf7686d6d5d9b91dfd3a06919047ea54f21ee2c
Edoardo Frulla <edofru01@gmail.com>
2021-08-29 10:09:24 +0200

    corretto input

--- grafica/classe.cpp
+++ grafica/classe.cpp
@@ -10,16 +10,21 @@ Mondo::Mondo(Inserimento t_inserimento)
       m_statica(sf::Vector2f(1800, 1800), sf::Vector2f(500, 500), t_inserimento.m_s_dimensione_lato, t_inserimento.m_s_parametro_beta,
                 t_inserimento.m_s_parametro_gamma, t_inserimento.m_s_parametro_eta, t_inserimento.m_s_infetti_iniziali,
                 t_inserimento.m_s_rimossi_iniziali),
-      m_window("test", sf::Vector2u(900, 600), &m_overlay, &m_dinamica, &m_statica, m_dinamica.get_bordi(), m_statica.get_bordi()) {}
+      m_window("test", sf::Vector2u(900, 600), &m_overlay, &m_dinamica, &m_statica, m_dinamica.get_bordi(), m_statica.get_bordi()) {
+
+
+      }
 
 template <class T>
-bool IsInputGood(T a) {
+bool IsInputGood(T& a) {
   std::string line;
   if (std::getline(std::cin, line)) {
     std::stringstream ss(line);
 
     if ((ss >> a) && ss.eof()) {
+      std::cout << a <<'\n';
       return true;
+
     }
   }
   return false;
@@ -31,36 +36,36 @@ Inserimento::Inserimento() {
   std::cout << "Automa Cellulare: \n";
 
   std::cout << "Numero di cellule per ogni lato >> ";
-  std::string line;
+  
   if (!IsInputGood(m_s_dimensione_lato)|| m_s_dimensione_lato < 0 || m_s_dimensione_lato > 10 ) {
 
     throw std::runtime_error{"Il parametro dimensione lato deve essere un intero compreso tra 0 e 10"};
   }
-
+std::cout << "dimensione lato"<< m_s_dimensione_lato <<'\n';
 
   std::cout << "Parametro beta(relativo alla probabilità di contagio) >> ";
-  if (!(std::cin >> m_s_parametro_beta) || m_s_parametro_beta < 0 || m_s_parametro_beta > 1) {
+  if (!IsInputGood(m_s_parametro_beta) || m_s_parametro_beta < 0 || m_s_parametro_beta > 1) {
     throw std::runtime_error{"Il parametro beta deve essere un decimale compreso tra 0 e 1"};
   }
 
   std::cout << "Parametro gamma(relativo alla probabilità di guarigione) >> ";
-  if (!(std::cin >> m_s_parametro_gamma) || m_s_parametro_gamma < 0 || m_s_parametro_gamma > 1) {
+  if (!IsInputGood(m_s_parametro_gamma) || m_s_parametro_gamma < 0 || m_s_parametro_gamma > 1) {
     throw std::runtime_error{"Il parametro gamma deve essere un decimale compreso tra 0 e 1"};
   }
 
   std::cout << "Parametro eta(relativo alla probabilità di morte) >> ";
-  if (!(std::cin >> m_s_parametro_eta) || m_s_parametro_eta < 0 || m_s_parametro_eta > 1) {
+  if (!IsInputGood(m_s_parametro_eta) || m_s_parametro_eta < 0 || m_s_parametro_eta > 1) {
     throw std::runtime_error{"La somma delle probabilità di morire e guarire deve essere compresa tra 0 e 1"};
   }
 
   std::cout << "Persone infette >> ";
-  if (!(std::cin >> m_s_infetti_iniziali) || m_s_infetti_iniziali < 0 || m_s_infetti_iniziali > m_s_dimensione_lato * m_s_dimensione_lato) {
+  if (!IsInputGood(m_s_infetti_iniziali) || m_s_infetti_iniziali < 0 || m_s_infetti_iniziali > m_s_dimensione_lato * m_s_dimensione_lato) {
     throw std::runtime_error{
         "Il numero di soggetti infetti deve essere un intero positivo e minore del numero massimo di persone presenti(lato*lato)"};
   }
 
   std::cout << "Persone rimosse(guarite) >> ";
-  if (!(std::cin >> m_s_rimossi_iniziali) || m_s_rimossi_iniziali < 0 ||
+  if (!IsInputGood(m_s_rimossi_iniziali) || m_s_rimossi_iniziali < 0 ||
       m_s_rimossi_iniziali > m_s_dimensione_lato * m_s_dimensione_lato - m_s_infetti_iniziali) {
     throw std::runtime_error{
         "Il numero di soggetti rimossi(guariti) deve essere un intero positivo e minore del numero massimo di persone ancora infettabili"};
@@ -69,27 +74,27 @@ Inserimento::Inserimento() {
   std::cout << "Animazione: \n";
 
   std::cout << "Numero di persone totali >> ";
-  if (!(std::cin >> m_d_numero_persone) || m_d_numero_persone < 0 || m_d_numero_persone > 100) {
+  if (!IsInputGood(m_d_numero_persone) || m_d_numero_persone < 0 || m_d_numero_persone > 100) {
     throw std::runtime_error{"Il parametro numero persone deve essere un intero compreso tra 0 e 100"};
   }
 
   std::cout << "Parametro beta(relativo alla probabilità di contagio) >> ";
-  if (!(std::cin >> m_d_parametro_beta) || m_d_parametro_beta < 0 || m_d_parametro_beta > 1) {
+  if (!IsInputGood(m_d_parametro_beta) || m_d_parametro_beta < 0 || m_d_parametro_beta > 1) {
     throw std::runtime_error{"Il parametro beta deve essere un decimale compreso tra 0 e 1"};
   }
 
   std::cout << "Parametro gamma(relativo alla probabilità di guarigione) >> ";
-  if (!(std::cin >> m_d_parametro_gamma) || m_d_parametro_gamma < 0 || m_d_parametro_gamma > 1) {
+  if (!IsInputGood(m_d_parametro_gamma) || m_d_parametro_gamma < 0 || m_d_parametro_gamma > 1) {
     throw std::runtime_error{"Il parametro gamma deve essere un decimale compreso tra 0 e 1"};
   }
 
   std::cout << "Persone infette >> ";
-  if (!(std::cin >> m_d_infetti_iniziali) || m_d_infetti_iniziali < 0 || m_d_infetti_iniziali > m_d_numero_persone) {
+  if (!IsInputGood(m_d_infetti_iniziali) || m_d_infetti_iniziali < 0 || m_d_infetti_iniziali > m_d_numero_persone) {
     throw std::runtime_error{"Il numero di soggetti infetti deve essere un intero positivo e minore del numero massimo di persone presenti"};
   }
 
   std::cout << "Persone rimosse(guarite) >> ";
-  if (!(std::cin >> m_d_rimossi_iniziali) || m_d_rimossi_iniziali < 0 || m_d_rimossi_iniziali > m_d_numero_persone - m_d_infetti_iniziali) {
+  if (!IsInputGood(m_d_rimossi_iniziali) || m_d_rimossi_iniziali < 0 || m_d_rimossi_iniziali > m_d_numero_persone - m_d_infetti_iniziali) {
     throw std::runtime_error{
         "Il numero di soggetti rimossi(guariti) deve essere un intero positivo e minore del numero massimo di persone ancora infettabili"};
   }
